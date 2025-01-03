5e3cb38ec6c9982bed4ae1062d43f5bcf0b027cf
green424minee <green424minee@gmail.com>
2025-01-03 12:16:43 +0900

    task3

--- src/kr/co/greenart/Jhu.java
+++ src/kr/co/greenart/Jhu.java
@@ -1,7 +1,9 @@
 package kr.co.greenart;
 
 public class Jhu {
-	// TODO 두 정수 plus
+	public int plus(int integer1, int integer2) {
+		return integer1 + integer2;
+	}
 	public int plus(int integer1, int integer2) {
 		return integer1 + integer2;
 	}
