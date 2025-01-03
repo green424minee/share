c9b4793e960efa5babf4ee4be86d5ec8d5d114e5
green424minee <green424minee@gmail.com>
2025-01-03 12:15:54 +0900

    task2

--- src/kr/co/greenart/민이.java
+++ src/kr/co/greenart/민이.java
@@ -1,11 +1,10 @@
 package kr.co.greenart;
 
 public class 민이 {
-	public int divide(int l, int r) {
-		return l / r;
-	}
-	
 	public double divide(double l, double r) {
 		return l / r;
+	}
+	public int divide(int left, int right) {
+		return left / right;
 	}
 }
