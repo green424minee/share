b056be38446f76c36fdd9379d3b475200ed2e77c
RI4RU <dlw0603@gmail.com>
2025-01-03 12:28:40 +0900

    update plus

--- src/kr/co/greenart/Jhu.java
+++ src/kr/co/greenart/Jhu.java
@@ -4,6 +4,7 @@ public class Jhu {
 	public int plus(int integer1, int integer2) {
 		return integer1 + integer2;
 	}
+	
 	public double plus(double double1, double double2) {
 		return double1 + double2;
 	}
