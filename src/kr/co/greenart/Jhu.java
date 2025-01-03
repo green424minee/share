8318db5020af23a4c63382ab5d5aec2e1bdbb30a
RI4RU <dlw0603@gmail.com>
2025-01-03 11:43:35 +0900

    두 실수의 덧셈 오버로드

--- src/kr/co/greenart/Jhu.java
+++ src/kr/co/greenart/Jhu.java
@@ -4,4 +4,8 @@ public class Jhu {
 	public int plus(int integer1, int integer2) {
 		return integer1 + integer2;
 	}
+	
+	public double plus(double double1, double double2) {
+		return double1 + double2;
+	}
 }
